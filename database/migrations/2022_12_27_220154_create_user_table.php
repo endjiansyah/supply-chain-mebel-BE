e12e8460c94b8c4bf2c8910d3a35809bd0702de2
endjiansyah <endji1507@gmail.com>
2022-12-28 06:02:50 +0700

    buat migration [role,user,kategori,status,barang,material,order,log]

---
+++ database/migrations/2022_12_27_220154_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_role');
+            $table->string('nama', 50);
+            $table->string('email', 100)->unique();
+            $table->text('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+};
---
+++ database/migrations/2022_12_27_221226_create_barang_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang', 100);
+            $table->text('deskripsi_barang')->nullable();
+            $table->integer('id_kategori');
+            $table->text('gambar');
+            $table->integer('panjang');
+            $table->integer('lebar');
+            $table->integer('tinggi');
+            $table->integer('id_material');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+};
